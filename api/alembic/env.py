7df8e829218ea1ccd6a19ae3ffe97161951823a9
akotsampaseris <a.kotsampaseris@gmail.com>
2026-01-23 14:27:24 +0200

    chore: make alembic support async

--- api/alembic/env.py
+++ api/alembic/env.py
@@ -1,13 +1,12 @@
-from logging.config import fileConfig
-
 import asyncio
+
+from alembic import context
+from logging.config import fileConfig
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-
-from alembic import context
-
 from app.core.database import Base
+from app.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -62,14 +61,14 @@ def run_migrations_online() -> None:
 
     """
 
-    async def do_run_migrations(connection):
+    def do_run_migrations(connection):
         context.configure(
             connection=connection,
             target_metadata=target_metadata
         )
 
-        async with context.begin_transaction():
-            await context.run_migrations()
+        with context.begin_transaction():
+            context.run_migrations()
 
     async def run_async_migrations():
         connectable = async_engine_from_config(
