eb00185179474caa3b64c02cc4ec7ec52f3201de
akotsampaseris <a.kotsampaseris@gmail.com>
2026-01-23 14:28:03 +0200

    chore: minor project changes

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -5,13 +5,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # App
     APP_NAME: str = "OpenAdopt"
     SECRET_KEY: str
     DEBUG: bool = False
 
+    # DB
     DATABASE_URL: str
+
+    # Auth
+    ACCESS_TOKEN_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 1440
     
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
+        extra = "ignore"
 
 settings = Settings()
